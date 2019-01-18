47f46b57fe2daa206f7ff0d360e7ef5a495e9ff1
Grizzly <grizzlyxyz@gmail>
2019-01-18 10:35:00 +0100

    om

---
+++ style.js
@@ -0,0 +1,58 @@
+// lieux -> Tableau
+// Ce sont les endroit ou l'on va se rendre
+//// Epicerie
+//// Maison
+//// Cuisine
+//// Panier
+
+// ingredients
+/// nom, etats ( entier,coupé, moulu, melangé, ...), prix
+//// Poivron
+//// Oignon
+//// Oeuf
+//// Epices
+////// Sel
+////// Poivre
+////// Paprika
+//// Fromage
+//// Huile/Beurre
+//// Salami
+
+
+// Personne
+//// Propriétés
+///// nom
+///// prenom
+///// age
+///// humeur
+///// argent
+//// Methods
+///// Casser_oeuf
+///// Battre_Oeuf
+///// Allumer_Cuisiniere
+///// Se_laver_mains
+///// Rajouter_ingredients
+///// Payer
+///// Deplacer 
+///// Laver
+
+
+// Cuisinière -> Objet
+// Ustensiles -> Objet
+//// Poele
+////// etat -> proriété
+////// chauffer -> method
+
+//// Fouet
+///// etat -> propriété
+//// Spatule
+///// etat -> propriété
+//// Assiette
+///// etat -> propriété (vide ,rempli,sale,propre)
+//// Bol
+///// etat -> propriété (vide ,rempli,sale,propre)
+//// Couverts
+//// etat -> sale , propre
+//// Lavabo
+//// etat -> vide ,rempli
+
